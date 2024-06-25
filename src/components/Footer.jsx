3ab980bdbbd9bd8364987c224c79487f6a17fe4d
Chhatish kumar <127961547+ChhatishK@users.noreply.github.com>
2024-06-26 00:23:52 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="mt-24">
+    <p className="dark:text-gray-200 text-gray-700 text-center m-20">
+      Welcome to My new React Project
+    </p>
+  </div>
+);
+
+export default Footer;
